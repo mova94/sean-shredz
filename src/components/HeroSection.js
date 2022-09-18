49bc3c4af7dd8be592f274b79549d6a41fb3f5ba
Nima Movasseghi <nmovasseghi94@gmail.com>
2022-09-17 23:23:56 -0700

    added cross fade for images on hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,13 +6,21 @@ import '../styling/HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>TRAIN TO BEAT GOKU NOW!</h1>
-      <p>What are you waiting for?</p>
+      <div className='picContainer'>
+        <div className='pic' id='pic1'> 
+          <h1>hello</h1> 
+        </div>
+        <div className='pic' id='pic2'> 
+          <h1>hello</h1> 
+        </div>
+        <div className='pic' id='pic3'> 
+          <h1>hello</h1> 
+        </div>
+      </div>
       <div className='hero-btns'>
         <Button
           className='btns'
-          buttonStyle='btn--outline'
+          buttonStyle='btn--primary'
           buttonSize='btn--large'
         >
           GET STARTED
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Sean Shredz
-            <i className='fab fa-heart' />
+            <img id='navLogo' src='images/logoTransparency.png' width="100%"/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
