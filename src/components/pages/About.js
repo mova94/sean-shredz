98bbc58391c185ca609d107f2f71e1f02b01d95f
Nima Movasseghi <nmovasseghi94@gmail.com>
2022-12-22 18:36:55 -0800

    redid about page. changed background to gradient.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import '../../App.css';
+import '../../styling/About.css';
 
 export default function About() {
   return(
     <div  className='about'>
-      {/* <h2 className='aboutHeader'>
-        Sean Maloney
-      </h2> */}
-      <div>
-        <p className='aboutText' id='text1'>   
-          I have been in the Health and Fitness industry since 2017. Formerly a Division 1 Lacrosse Athlete, I took an interest in the Health and Fitness industry. I found fulfillment from improving people’s lives through fitness.
-        </p>
-        <p className='aboutText' id='text2'>
-         Throughout this time, I have delivered life-changing results to clients. I put my training methods to the test as a Competitive Bodybuilder to the test. 
-        </p>
-      </div>
-      <div>
-        
+      <div className='about_container'>
+        <div className='left_side'>
+          <img className='about_image' src="images/comp2.jpg" alt="Sean Comp Medals"  />
+        </div>
+        <div className='right_side'>
+            <p className='aboutText'>  
+              I have been in the Health and Fitness industry since 2017. Formerly a Division 1 Lacrosse Athlete, I took an interest in the Health and Fitness industry. I found fulfillment from improving people’s lives through fitness.
+              Throughout this time, I have delivered life-changing results to clients. I put my training methods to the test as a Competitive Bodybuilder to the test. 
+                </p>
+        </div>
       </div>
     </div>
     )
 }
+
+        {/* <p  id='text1'>   
+        
+        </p> */}
