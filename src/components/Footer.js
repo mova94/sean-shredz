221ce0618bac0d2131a923186770a6d381d882cc
Nima Movasseghi <nmovasseghi94@gmail.com>
2023-01-24 19:50:05 -0800

    Finished wiring up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,115 +1,40 @@
-import React from 'react';
-import '../styling/Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styling/Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
-              <i className='fab fa-typo3' />
-            </Link>
-          </div>
-          <small className='website-rights'>TRVL © 2020</small>
-          <div className='social-icons'>
-            <Link
-            className='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+    <div className="footer-container">
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="social-icons">
+            <a
+              className="social-icon-link tiktok"
+              href="https://www.tiktok.com/@sean.shredzz"
+              target="_blank"
+              rel="noopener"
+              aria-label="TikTok"
             >
-              <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
-            className='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              <i className="fab fa-tiktok" />
+            </a>
+            <a
+              class="social-icon-link facebook"
+              href="https://www.facebook.com/sean.maloney.1447?mibextid=LQQJ4d"
+              target="_blank"
+              rel="noopener"
+              aria-label="Facebook"
             >
-              <i className='fab fa-instagram' />
-            </Link>
-            <Link
-            className='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
+              <i class="fab fa-facebook" />
+            </a>
+            <a
+              class="social-icon-link instagram"
+              href="https://instagram.com/sean.shredz"
+              target="_blank"
+              rel="noopener"
+              aria-label="Instagram"
             >
-              <i className='fab fa-youtube' />
-            </Link>
-            <Link
-            className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i className='fab fa-twitter' />
-            </Link>
-            <Link
-            className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i className='fab fa-linkedin' />
-            </Link>
+              <i class="fab fa-instagram" />
+            </a>
           </div>
         </div>
       </section>
